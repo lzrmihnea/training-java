9628eb966bf057a0cf66ea5eeb788860419b753d
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-12-15 21:41:51 +0200

    Course 12: Sent param through controller endpoint

--- training-spring/src/main/java/org/fasttrackit/trainingspring/controller/HelloController.java
+++ training-spring/src/main/java/org/fasttrackit/trainingspring/controller/HelloController.java
@@ -1,14 +1,16 @@
 package org.fasttrackit.trainingspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/salut")
-    public String getHelloMessage() {
-        return "Salut lume!";
+    @GetMapping("/api/salut/{mesaj}")
+    public String getHelloMessage(@PathVariable("mesaj") String pathVar) {
+        String responseText = "<h1 style=color:green> Salut lume! </h1> \n <h4>Bun venit pe serverul nostru local!</h4>\n " + pathVar;
+        return responseText;
     }
 
 }
